d6b4664f4eb1b0570614b882e02e71145839e51b
h80r <h80rgbc@gmail.com>
2022-12-10 00:03:02 -0300

    feat: Implement loading transition

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,30 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
+	[SerializeField]
+	private Image spinner;
 	public void StartGame()
 	{
-		SceneManager.LoadScene(gameObject.scene.buildIndex + 1);
+		StartCoroutine(LoadGameSceneAsync());
 	}
+	private IEnumerator LoadGameSceneAsync()
+    {
+        // Start the asynchronous load operation
+        var loadOperation = SceneManager.LoadSceneAsync(gameObject.scene.buildIndex + 1);
+    
+        // Wait until the asynchronous load operation has completed
+        while (!loadOperation.isDone)
+        {
+            // Update the fillAmount property of the Image component to reflect the loading progress
+            spinner.fillAmount = Math.Min(loadOperation.progress + 0.3f, 1.0f);
+    
+            // Wait for the next frame
+            yield return null;
+        }
+    }
 }
